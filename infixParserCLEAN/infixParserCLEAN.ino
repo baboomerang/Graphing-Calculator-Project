1a78e76b8b34cbbd02b454bd0ddcccd615a77b84
baboomerang <irodrigue101@gmail.com>
2017-03-07 02:10:25 -0500

    Improved code visiblity and more efficient processing

--- infixParserCLEAN/infixParserCLEAN.ino
+++ infixParserCLEAN/infixParserCLEAN.ino
@@ -1,7 +1,7 @@
 //input / cin variables
 char byteChar;
 
-//Cin input X location of infix expression (infX) printing from left to right 
+//Cin input X location of infix expression (infX) printing from left to right
 unsigned int infX = 0;
 // saved number array position in order ie. numberStack_FINAL[3] = "2323212" where num_indx = 3
 unsigned int num_indx = 0;
@@ -56,7 +56,7 @@ void infixproc() {  // INFIX PROC DOES EXACTLY WHAT GETLINE DOES IN C++, but ard
       char procChar = char(infixstring[i]);
       switch (procChar) {
         case '0' ... '9':
-          infixRAWnumberStack[num_x] = procChar;  //  infix string of 345+96-22/7-999 will become rawnumberstack of 34596227999 and depending on the distance between the sequence of numbers to the nearest operator, we can delimit and "cut" each number out of this raw stack like a stencil. 
+          infixRAWnumberStack[num_x] = procChar;  //  infix string of 345+96-22/7-999 will become rawnumberstack of 34596227999 and depending on the distance between the sequence of numbers to the nearest operator, we can delimit and "cut" each number out of this raw stack like a stencil.
           num_x += 1;
           break;
         case '(':
@@ -66,12 +66,12 @@ void infixproc() {  // INFIX PROC DOES EXACTLY WHAT GETLINE DOES IN C++, but ard
         case ')':
           Serial.println("num_x : " + String(num_x) + " | " + "parenthcount : " + String(openparenth_count) + " | " + "cutHere : " + String(cutHere) + " | ");
           openparenth_count -= 1;
-          save_op(7);
           if ( cutHere != num_x ) {
             cutHere = num_x;
             Serial.println("DEBUG: save num INVOKED BY CLOSING PARENTHESIS");
             save_num(start, cutHere, num_indx);
           }
+          save_op(7); // this line IS VERY ORDER SENSITIVE, DO NOT CHANGE THE ORDER OTHERWISE STUFF BREAKS. THIS LINE HAS TO BE HERE
           if (openparenth_count == 0) {
             Serial.println("PARENTH COUNT IS 0");
             syntax_check(i); //=================================================== as a terrible design choice, this whole program continues by calling the syntax check soubroutine
@@ -128,11 +128,11 @@ void infixproc() {  // INFIX PROC DOES EXACTLY WHAT GETLINE DOES IN C++, but ard
       } // end of switch case
     } // end of for loop
     Serial.println("Recieved infix string: " + String(infixstring));
-    
+
     for (int i = 0; i < (sizeof(infix_stack_reference) / sizeof(int)); i++) {
       Serial.print(String(infix_stack_reference[i]));
     } Serial.println("");
-  
+
   } //end of if character g
 } //end of infixproc
 
@@ -140,7 +140,7 @@ void save_num(int start, int cutpoint, int index_xpos) {
   save_op(1);
   String Z = String(infixRAWnumberStack);
   String Zshort = Z.substring(start, cutpoint);
-  //the cutpoint does not include the value at the cutpoint. its an exclusion limit. if we had 35456 and cut point was 4 with start 0, we would just get the first 3
+  //the cutpoint does not include the value at the cutpoint. its an exclusion limit. if we had 97806 and cut point was 4 digits with start 0, we would just get the first 3 (978)
   long i = Zshort.toInt();
   numberStack_FINAL[index_xpos] = i;
   num_indx++;
@@ -203,10 +203,6 @@ void calculate_postfix() {
   for ( int p = ((sizeof(postfix_stack_reference) - 1) / sizeof(int)) ; p >= 0 ; p--) {
     Serial.println("postfix_stack_reference[" + String(p) + "] =  " + String(postfix_stack_reference[p]));
   }
-  //  for (int p = 0; lol < ((sizeof(infix_stack_reference)) / sizeof(int)); lol++) {
-  //    Serial.println(" ~~~~~?@#!#!@$?!@$!@$!@#>!@3?!>@#   CALC PROC POSITION " + String(lol) + " TYPE STATUS:   " + String(infix_stack_reference[lol]) );
-  //    if (infix_stack_reference[lol] == 0) break;
-  //  }
 }
 void copy(byte location, byte input ) {
   postfix_stack_reference[location] = input;
@@ -214,11 +210,6 @@ void copy(byte location, byte input ) {
 }
 
 void pushtostack(byte precedence, int opr8tr) {
-  for (int i = 0; ( i < (sizeof(postfix_opstack) / sizeof(int))); i++) {
-    Serial.print(String(postfix_opstack[i]));
-  }
-  Serial.println("");
-  delay(25);
   Serial.println("precedence: " + String(precedence) + " operator value: " + String(opr8tr));
   //some funky stuff with checking p = 1 and it wont override it at all due to some funky stuff. Any value after p == 1 breaks the code chain and dupes the top op code for some stupid reason. go fix that bro
   for ( int p = ((sizeof(postfix_opstack) - 1) / sizeof(int)) ; p >= 0 ; p--) {
@@ -245,9 +236,8 @@ void pushtostack(byte precedence, int opr8tr) {
       postfix_opstack[ p + 1 ] = opr8tr;
       break;
     } else if (precedence == 255 && opr8tr == 7) {
-      delay(50);
-      Serial.println("we have for pp: " + String(p) + "   " + "for postfix_opstack[pp], we have : " + String(postfix_opstack[p]));
       if (postfix_opstack[p] != 0) {
+        Serial.println("we have for pp: " + String(p) + "   " + "for postfix_opstack[pp], we have : " + String(postfix_opstack[p]));
         int operator_2b_popped = postfix_opstack[p];
         postfix_opstack[p] = 0;
         if (operator_2b_popped != 6) {
