c902e53244ae48d757e391d9935c313d55678311
Ilan <irodrigue101@gmail.com>
2020-10-05 23:38:34 -0400

    inlined math function
    refactored math function and loop when its at the end of input stream.

--- calculate.ino
+++ calculate.ino
@@ -1,15 +1,13 @@
 void calculate(char (&input)[INPUT_SIZE]) {
   String num;
   num.reserve(INPUT_SIZE);
-  Stack<char> opstack(INPUT_SIZE / 2);
+  Stack<char> opstack(INPUT_SIZE / 2);  //binary operators require 2 operands
   List<BigNumber> operands;
 
   for (int x = 0; x <= index; x++) {
     char token = input[x];
-
     if (isDigit(token) || token == '.') {
       num.concat(token);
-
     } else {
       if (num)
         transfer(num, operands);
@@ -18,37 +16,37 @@ void calculate(char (&input)[INPUT_SIZE]) {
         while (precedence(token) <= precedence(opstack.peek())) {
           if (associativity(token))
             break;  //warning: clever use of break; only when '^' is pushed to another '^'
-          if (operands.count() >= 2)
-            doMath(opstack.pop(), operands); //must do math with higher precedence operators first
+          doMath(opstack.pop(), operands); //must do math with higher precedence operators first
         }
         opstack.push(token);
       }
 
-      if (token == '(') {
+      if (token == '(')
         opstack.push(token);
-      }
 
       if (token == ')') {
-        for (char token = opstack.pop(); token != (NULL || '('); token = opstack.pop()) {
-          if (operands.count() >= 2)
-            doMath(token, operands);
-        }
+        while (doMath(opstack.peek(), operands) == 0)
+          opstack.pop();
       }
 
     }
   }
 
-  while (!opstack.isEmpty()) {
-    if (operands.count() >= 2)
-      doMath(opstack.pop(), operands);
-    else
-      break;  //if operand stack is too small, break the infinite loop
+  //above logic only runs while theres characters to read.
+  //we must do one final check at the end of the input stream to get proper output
+
+  while (doMath(opstack.peek(), operands) == 0) {
+    opstack.pop();  //if do math was a success, remove it from the stack.
   }
 
-  if (!opstack.isEmpty())
+  if (!opstack.isEmpty()) {
     raise(2);  //opstack should have been empty if syntax was respected
-  if (operands.count() != 1)
-    raise(3);  //if parenthesis were abused, we would get these errors
+    return;
+  }
+  if (operands.count() != 1) {
+    raise(3);  //if parenthesis were abused, there werent enough operators
+    return;
+  }
 
   auto final = operands.pop();
   printBignum(final);
@@ -62,7 +60,10 @@ void transfer(String &num, List<BigNumber> &output) {
   num.remove(0);
 }
 
-void doMath(char op, List<BigNumber> &output) {
+inline uint8_t doMath(char op, List<BigNumber> &output) {
+  if (output.count() < 2) {
+    return 1;  //if there arent enough operands, return error
+  }
   BigNumber y = output.pop();
   BigNumber z = output.pop();
   switch (op) {
@@ -81,10 +82,12 @@ void doMath(char op, List<BigNumber> &output) {
     case ('^'):
       z.pow(y);
       break;
+    default:
+      return 1;  //catch all if parenths get caught in the case
   }
   output.push(z);
+  return 0;
 }
-
 bool isOperator(char &value) {
   switch (value) {
     case '+':
